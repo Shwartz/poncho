847192ce45e360dbc5454f6f0002230e00dccdf8
andris.svarcs <andris.svarcs@avanade.com>
2021-02-21 15:13:52 +0000

    Refactoring main menu into its own component

--- src/components/MainMenuItem.tsx
+++ src/components/MainMenuItem.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export interface IMainMenuItem {
-  cssClasses: string;
   pathTo: string;
   title: string;
 }
 
-export const MainMenuItem = (props: {items: IMainMenuItem[]}) => {
-  const { items } = props;
+export const MainMenuItem = (props: {items: IMainMenuItem[], cssClasses: string}) => {
+  const { items, cssClasses } = props;
 
   return (
     <React.Fragment>
-      {items.map(item => <Link key={item.title} to={item.pathTo} className={item.cssClasses}>{item.title}</Link>)}
+      {items.map(item => <Link key={item.title} to={item.pathTo} className={cssClasses}>{item.title}</Link>)}
     </React.Fragment>
   )
 }
